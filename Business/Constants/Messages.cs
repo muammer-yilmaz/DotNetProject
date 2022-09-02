75d6f9ac79c1f46685d7e685c585aa1409598613
Muammer Yilmaz <muammercan68@gmail.com>
2022-09-02 17:09:13 +0300

    Jwt added

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +12,15 @@ namespace Business.Constants {
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
         public static string ProductListed = "Ürünler listelendi";
-        internal static string ProductCountOfCategoryError = "Kategoride bulunabilecek en fazla ürün limiti doldu";
-        internal static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var";
-        internal static string CategoryLimitExceeded = "Bulunabilecek maksimum kategori sayısı aşıldı";
+        public static string ProductCountOfCategoryError = "Kategoride bulunabilecek en fazla ürün limiti doldu";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var";
+        public static string CategoryLimitExceeded = "Bulunabilecek maksimum kategori sayısı aşıldı";
+        public static string AuthorizationDenied = "Yetkiniz yok";
+        internal static string UserRegistered;
+        internal static User UserNotFound;
+        internal static User PasswordError;
+        internal static string SuccessfulLogin;
+        internal static string UserAlreadyExists;
+        internal static string AccessTokenCreated;
     }
 }
